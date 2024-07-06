6496af6a6b44fe6a46fad784ff0ed0792ae0612d
Victor Shevtsov <victorshevtsov@gmail.com>
2024-07-06 15:21:57 +0000

    Add build.rs and proto

---
+++ build.rs
@@ -0,0 +1,25 @@
+use pathsearch::find_executable_in_path;
+use protobuf_zmq_rust_generator::ZmqServerGenerator;
+use std::io::Result;
+
+macro_rules! p {
+	($($tokens: tt)*) => {
+			println!("cargo:warning={}", format!($($tokens)*))
+	}
+}
+
+fn main() -> Result<()> {
+    if let Some(exe) = find_executable_in_path("protoc") {
+        let path_to_exe = exe.display().to_string();
+        p!("Found protoc compiler at {}", path_to_exe);
+        std::env::set_var("PROTOC", path_to_exe);
+        prost_build::Config::new()
+            .out_dir("src/message/")
+            .service_generator(Box::new(ZmqServerGenerator {}))
+            .compile_protos(&["prover.proto"], &["proto/"])?;
+    } else {
+        p!("Cannot find protoc compiler on operating system. Please install protoc");
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod message;
+
 fn main() {
     println!("Hello, world!");
 }
