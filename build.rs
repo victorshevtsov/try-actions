eff6d60ca0963b2d27ef7c60712668d5aa827ec1
Victor Shevtsov <victorshevtsov@gmail.com>
2024-07-07 09:01:10 +0000

    Update build.rs

--- build.rs
+++ build.rs
@@ -12,11 +12,11 @@ fn main() -> Result<()> {
     if let Some(exe) = find_executable_in_path("protoc") {
         let path_to_exe = exe.display().to_string();
         p!("Found protoc compiler at {}", path_to_exe);
-        std::env::set_var("PROTOC", path_to_exe);
-        prost_build::Config::new()
-            .out_dir("src/message/")
-            .service_generator(Box::new(ZmqServerGenerator {}))
-            .compile_protos(&["prover.proto"], &["proto/"])?;
+        // std::env::set_var("PROTOC", path_to_exe);
+        // prost_build::Config::new()
+        //     .out_dir("src/message/")
+        //     .service_generator(Box::new(ZmqServerGenerator {}))
+        //     .compile_protos(&["prover.proto"], &["proto/"])?;
     } else {
         p!("Cannot find protoc compiler on operating system. Please install protoc");
     }
